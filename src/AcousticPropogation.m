c9016b0121089b755fe2b8232833f34c718c540e
Rakshit <rakki22k@gmail.com>
2021-05-21 15:56:16 +0530

    Simulate optical and acoustic propogation
    Simulated optical propogation and found the fluence distribution.
    Then simulated the acoustic propogation with the energy distribution as the source.

---
+++ src/AcousticPropogation.m
@@ -0,0 +1,50 @@
+
+
+% load the initial pressure distribution from an image and scale the
+% magnitude
+GruneisenParameter = 3;
+p0 = GruneisenParameter * loadImage('./Results/energy_distribution_H.jpg');
+
+% create the computational grid
+Nx = 128;           % number of grid points in the x (row) direction
+Ny = 128;           % number of grid points in the y (column) direction
+dx = 0.1e-3;        % grid point spacing in the x direction  [m]
+dy = 0.1e-3;        % grid point spacing in the y direction  [m]
+kgrid = kWaveGrid(Nx, dx, Ny, dy);
+
+% resize the image to match the size of the computational grid and assign
+% to the source input structure
+source.p0 = resize(p0, [Nx, Ny]);
+
+% define the properties of the propagation medium
+medium.sound_speed = 1500;  % [m/s]
+medium.alpha_coeff = 0.75;  % [dB/(MHz^y cm)]
+medium.alpha_power = 1.5;
+
+% define a centered circular sensor
+sensor_radius = 4e-3;   % [m]
+num_sensor_points = 50;
+sensor.mask = makeCartCircle(sensor_radius, num_sensor_points);
+
+% run the simulation
+sensor_data = kspaceFirstOrder2D(kgrid, medium, source, sensor);
+
+% =========================================================================
+% VISUALISATION
+% =========================================================================
+
+% plot the initial pressure and sensor distribution
+% figure;
+% imagesc(kgrid.y_vec * 1e3, kgrid.x_vec * 1e3, source.p0 + cart2grid(kgrid, sensor.mask), [-1, 1]);
+% colormap(getColorMap);
+% ylabel('x-position [mm]');
+% xlabel('y-position [mm]');
+% axis image;
+
+% plot the simulated sensor data
+figure;
+imagesc(sensor_data, [-1, 1]);
+colormap(getColorMap);
+ylabel('Sensor Position');
+xlabel('Time Step');
+colorbar;
--- src/optical_propogation.m
+++ src/optical_propogation.m
@@ -1,4 +1,51 @@
 
-Mesh = load_mesh('./MeshSample/mesh4WithAnamoly');
 
-fluence_data = femdata('./MeshSample/mesh4WithAnamoly',100);
+Mesh = load_mesh('./MeshSample/mesh5.node');
+
+fluence_data = femdata('./MeshSample/mesh5.node',0);
+
+%Convert sparse matrix to full matrix 
+fluence_data.phi = full(fluence_data.phi);
+
+H = fluence_data.phi.*Mesh.mua;
+
+% subplot(1,4,1);
+figure;
+plotim(Mesh,Mesh.mua);
+title('\mu_a','FontSize',20);
+colorbar('horiz');
+
+% subplot(1,4,2);
+figure;
+plotim(Mesh,Mesh.mus);
+title('\mu_s','FontSize',20);
+colorbar('horiz');
+
+figure;
+plotim(Mesh,fluence_data.phi);
+title('Fluence','FontSize',20);
+colorbar('horiz');
+
+figure;
+plotim(Mesh,H);
+title('Energy distribution','FontSize',20);
+colorbar('horiz');
+
+
+
+%% plot image function
+function plotim(mesh,val)
+
+       h = trisurf(mesh.elements,...
+	    mesh.nodes(:,1),...
+	    mesh.nodes(:,2),...
+	    mesh.nodes(:,3),...
+	    val);
+
+shading interp;
+view(2);
+axis equal; 
+axis off;
+colormap hot;
+
+end
